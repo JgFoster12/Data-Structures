3a07be124715e8a01eb15475cdf2ccf2efe64355
JGFoster12 <jakefoster@protonmail.com>
2020-01-13 09:32:56 +0700

    First commit

---
+++ Tree.java
@@ -0,0 +1,6 @@
+public interface Tree<D extends Comparable> {
+    public boolean isEmpty();
+    public int cardinality();
+    public boolean member(D element);
+    public NonEmptyBST<D> add(D element);
+}
